a86beda23fb009904daf05ac2bfffdb56ed482ec
zhoujumbo <2505148618@qq.com>
2023-07-06 21:02:48 +0800

    303 06

---
+++ src/main/java/leetcode/p300/E303.java
@@ -0,0 +1,53 @@
+package leetcode.p300;
+
+public class E303 {
+
+    public E303(int[] nums) {
+
+    }
+
+    public int sumRange(int left, int right) {
+
+        
+        return 1;
+    }
+
+}
+
+
+/**
+ * 给定一个整数数组 nums，处理以下类型的多个查询:
+ *
+ * 计算索引left和right（包含 left 和 right）之间的 nums 元素的 和 ，其中left <= right
+ * 实现 NumArray 类：
+ *
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums中索引left和right之间的元素的 总和 ，包含left和right两点（也就是nums[left] + nums[left + 1] + ... + nums[right])
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ *
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ * 
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 104
+ * -105<= nums[i] <=105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/range-sum-query-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
